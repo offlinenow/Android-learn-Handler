dd4b47f73e912937fd846a7184ad1745b45f49c7
offlinenow <offlinenow@users.noreply.github.com>
2017-09-15 16:11:57 +0800

    Thread

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,48 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
-    MyHandlerThread myHandlerThread = null;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            String string = msg.obj.toString();
+            TextView textView = findViewById(R.id.TextView1);
+            textView.setText(string);
+        }
+    };
+    final Runnable mywork = new Runnable() {
+        @Override
+        public void run() {
+            Looper.prepare();
+            Message message = Message.obtain();
+            message.obj = "New Thread";
+            message.what = 10;
+            handler.sendMessage(message);
+            Looper.loop();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void login(View view) {
-        myHandlerThread = new MyHandlerThread("onStartHandlerThread");
+        Thread thread = new Thread(mywork);
+        thread.start();
 
-        myHandlerThread.start();
-        Log.i("Old Thread", "old Thread");
+
     }
 
     public void PopUpWindow(View view) {
