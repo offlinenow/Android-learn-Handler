76a966632306858f5936aa699e42facaea886c67
offlinenow <offlinenow@users.noreply.github.com>
2017-09-15 15:01:25 +0800

    Handler

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public final static String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
+    MyHandlerThread myHandlerThread = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
+        myHandlerThread = new MyHandlerThread("onStartHandlerThread");
 
+        myHandlerThread.start();
+        Log.i("Old Thread", "old Thread");
     }
 
     public void PopUpWindow(View view) {
